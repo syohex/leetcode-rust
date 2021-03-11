37a9ce7ac7f3646f5a9e76c258c2c934aeb89224
Shohei YOSHIDA <syohex@gmail.com>
2021-03-11 21:28:27 +0900

    Add coin change

---
+++ challenge/202103/coin_change/src/main.rs
@@ -0,0 +1,47 @@
+fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+    let mut dp = vec![amount + 1; (amount + 1) as usize];
+    dp[0] = 0;
+
+    for i in 1..=amount {
+        for &coin in &coins {
+            if i >= coin {
+                dp[i as usize] = std::cmp::min(dp[i as usize], dp[(i - coin) as usize] + 1);
+            }
+        }
+    }
+
+    if dp[amount as usize] == amount + 1 {
+        -1
+    } else {
+        dp[amount as usize]
+    }
+}
+
+fn main() {
+    let coins = vec![1, 2, 5];
+    println!("coin_change([1, 2, 5], 11)={}", coin_change(coins, 11));
+}
+
+#[test]
+fn test_coin_change() {
+    {
+        let coins = vec![1, 2, 5];
+        assert_eq!(coin_change(coins, 11), 3);
+    }
+    {
+        let coins = vec![2];
+        assert_eq!(coin_change(coins, 3), -1);
+    }
+    {
+        let coins = vec![1];
+        assert_eq!(coin_change(coins, 0), 0);
+    }
+    {
+        let coins = vec![1];
+        assert_eq!(coin_change(coins, 1), 1);
+    }
+    {
+        let coins = vec![1];
+        assert_eq!(coin_change(coins, 2), 2);
+    }
+}

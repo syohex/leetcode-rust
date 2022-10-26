1f424b83b1c259767f0f143ce49bd99b904e45b1
Shohei YOSHIDA <syohex@gmail.com>
2022-10-26 11:30:51 +0900

    Accepted using HashMap and O(n) algorithm

--- challenge/202210/continuous_subarray_sum/src/main.rs
+++ challenge/202210/continuous_subarray_sum/src/main.rs
@@ -1,17 +1,21 @@
 fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
-    let len = nums.len();
-    let mut acc = vec![0; len + 1];
+    use std::collections::HashMap;
 
-    for i in 0..len {
-        acc[i + 1] = acc[i] + nums[i];
-    }
+    let mut h: HashMap<i32, usize> = HashMap::new();
+    h.insert(0, 0);
 
-    for i in 2..=len {
-        for j in 0..=(i - 2) {
-            let sum = acc[i] - acc[j];
-            if sum % k == 0 {
+    let mut sum = 0;
+
+    for (i, num) in nums.into_iter().enumerate() {
+        sum += num;
+
+        let m = sum % k;
+        if let Some(j) = h.get(&m) {
+            if *j < i {
                 return true;
             }
+        } else {
+            h.insert(m, i + 1);
         }
     }
 
@@ -26,21 +30,21 @@ fn main() {
 
 #[test]
 fn test_check_subarray_sum() {
-    // {
-    //     let nums = vec![23, 2, 4, 6, 7];
-    //     let ret = check_subarray_sum(nums, 6);
-    //     assert!(ret);
-    // }
-    // {
-    //     let nums = vec![23, 2, 6, 4, 7];
-    //     let ret = check_subarray_sum(nums, 6);
-    //     assert!(ret);
-    // }
-    // {
-    //     let nums = vec![23, 2, 6, 4, 7];
-    //     let ret = check_subarray_sum(nums, 13);
-    //     assert!(!ret);
-    // }
+    {
+        let nums = vec![23, 2, 4, 6, 7];
+        let ret = check_subarray_sum(nums, 6);
+        assert!(ret);
+    }
+    {
+        let nums = vec![23, 2, 6, 4, 7];
+        let ret = check_subarray_sum(nums, 6);
+        assert!(ret);
+    }
+    {
+        let nums = vec![23, 2, 6, 4, 7];
+        let ret = check_subarray_sum(nums, 13);
+        assert!(!ret);
+    }
     {
         let nums = vec![0, 0];
         let ret = check_subarray_sum(nums, 1);

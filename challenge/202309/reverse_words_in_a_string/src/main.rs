3ebcd95e958021cc15937c01fc1061bbf12464df
Shohei YOSHIDA <syohex@gmail.com>
2023-10-01 11:13:14 +0900

    Add reverse words in a string 3

---
+++ challenge/202309/reverse_words_in_a_string/src/main.rs
@@ -0,0 +1,28 @@
+fn reverse_words(s: String) -> String {
+    s.split_whitespace()
+        .map(|s| s.chars().rev().collect())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
+    let s = "Let's take LeetCode contest".to_string();
+    let ret = reverse_words(s);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test_reverse_words() {
+    {
+        let s = "Let's take LeetCode contest".to_string();
+        let expected = "s'teL ekat edoCteeL tsetnoc".to_string();
+        let ret = reverse_words(s);
+        assert_eq!(ret, expected);
+    }
+    {
+        let s = "God Ding".to_string();
+        let expected = "doG gniD".to_string();
+        let ret = reverse_words(s);
+        assert_eq!(ret, expected);
+    }
+}

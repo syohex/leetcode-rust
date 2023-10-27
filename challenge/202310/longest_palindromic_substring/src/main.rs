e327a203d49752ca51f3df1eed070b943a97a89d
Shohei YOSHIDA <syohex@gmail.com>
2023-10-27 10:52:36 +0900

    Add longest palindromic substring

---
+++ challenge/202310/longest_palindromic_substring/src/main.rs
@@ -0,0 +1,52 @@
+fn longest_palindrome(s: String) -> String {
+    let cs: Vec<char> = s.chars().collect();
+    let len = s.len();
+
+    let mut dp = vec![vec![false; len]; len];
+    let mut start = 0;
+    let mut end = 0;
+    for i in 0..len {
+        dp[i][i] = true;
+    }
+
+    for i in 0..(len - 1) {
+        if cs[i] == cs[i + 1] {
+            dp[i][i + 1] = true;
+            start = i;
+            end = i + 1;
+        }
+    }
+
+    for i in 2..len {
+        let limit = len - i;
+        for j in 0..limit {
+            if cs[j] == cs[j + i] && dp[j + 1][j + i - 1] {
+                dp[j][j + i] = true;
+                start = j;
+                end = j + i;
+            }
+        }
+    }
+
+    s[start..=end].to_string()
+}
+
+fn main() {
+    let s = "cbbd".to_string();
+    let ret = longest_palindrome(s);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test_longest_palindrome() {
+    {
+        let s = "babad".to_string();
+        let ret = longest_palindrome(s);
+        assert_eq!(ret, "aba");
+    }
+    {
+        let s = "cbbd".to_string();
+        let ret = longest_palindrome(s);
+        assert_eq!(ret, "bb");
+    }
+}

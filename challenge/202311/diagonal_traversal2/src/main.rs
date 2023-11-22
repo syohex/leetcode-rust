1089e1f83262d1256cc5825dd5a43418c8ef8c6b
Shohei YOSHIDA <syohex@gmail.com>
2023-11-22 15:24:15 +0900

    Accepted

--- challenge/202311/diagonal_traversal2/src/main.rs
+++ challenge/202311/diagonal_traversal2/src/main.rs
@@ -1,42 +1,20 @@
 fn find_diagonal_order(nums: Vec<Vec<i32>>) -> Vec<i32> {
-    let rows = nums.len();
-    let cols = nums.iter().fold(0, |acc, v| {
-        std::cmp::max(acc, v.len())
-    });
+    use std::collections::HashMap;
 
-    let mut ret = vec![];
-    for i in 0..rows {
-        let mut i = i as i32;
-
-        let mut j = 0;
-        loop {
-            if i < 0 {
-                break;
-            }
-
-            if let Some(&v) = nums[i as usize].get(j) {
-                ret.push(v);
-            }
-
-            i -= 1;
-            j += 1;
+    let mut h = HashMap::new();
+    for i in 0..nums.len() {
+        for j in 0..nums[i].len() {
+            h.entry(i + j).or_insert(vec![]).push(nums[i][j]);
         }
     }
 
-    for mut j in 1..cols {
-        let mut i = (rows - 1) as i32;
-        loop {
-            if i < 0 {
-                break;
-            }
-
-            if let Some(&v) = nums[i as usize].get(j) {
-                ret.push(v);
-            }
-
-            i -= 1;
-            j += 1;
+    let mut ret = vec![];
+    let mut i = 0;
+    while let Some(v) = h.get(&i) {
+        for n in v.iter().rev() {
+            ret.push(*n);
         }
+        i += 1;
     }
 
     ret

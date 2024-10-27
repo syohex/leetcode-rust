fa193346333db6d98e6925e390d60f3f3f40798b
Shohei YOSHIDA <syohex@gmail.com>
2024-10-27 10:06:16 +0900

    Add count square submatrices with all ones

---
+++ challenge/202410/count_square_submatrices_with_all_ones/src/main.rs
@@ -0,0 +1,37 @@
+fn count_squares(matrix: Vec<Vec<i32>>) -> i32 {
+    let (rows, cols) = (matrix.len(), matrix[0].len());
+    let mut dp = vec![vec![0; cols + 1]; rows + 1];
+
+    let mut ret = 0;
+    for i in 0..rows {
+        for j in 0..cols {
+            if matrix[i][j] == 1 {
+                dp[i + 1][j + 1] =
+                    1 + std::cmp::min(dp[i][j], std::cmp::min(dp[i + 1][j], dp[i][j + 1]));
+                ret += dp[i + 1][j + 1];
+            }
+        }
+    }
+
+    ret
+}
+
+fn main() {
+    let matrix = vec![vec![0, 1, 1, 1], vec![1, 1, 1, 1], vec![0, 1, 1, 1]];
+    let ret = count_squares(matrix);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test() {
+    {
+        let matrix = vec![vec![0, 1, 1, 1], vec![1, 1, 1, 1], vec![0, 1, 1, 1]];
+        let ret = count_squares(matrix);
+        assert_eq!(ret, 15);
+    }
+    {
+        let matrix = vec![vec![1, 0, 1], vec![1, 1, 0], vec![1, 1, 0]];
+        let ret = count_squares(matrix);
+        assert_eq!(ret, 7);
+    }
+}

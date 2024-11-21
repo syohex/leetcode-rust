b2e6ba424cb26103bffbff1ea6f7d856be6a9f07
Shohei YOSHIDA <syohex@gmail.com>
2024-11-21 12:20:15 +0900

    Add count unguarded cells in the grid but TLE

---
+++ challenge/202411/count_unguarded_cells_in_the_grid/src/main.rs
@@ -0,0 +1,90 @@
+fn count_unguarded(m: i32, n: i32, guards: Vec<Vec<i32>>, walls: Vec<Vec<i32>>) -> i32 {
+    #[derive(Debug, Clone, Eq, PartialEq)]
+    enum State {
+        UnGuarded,
+        Wall,
+        Guarded,
+    }
+
+    let (m, n) = (m as usize, n as usize);
+    let mut matrix = vec![vec![State::UnGuarded; n]; m];
+
+    for wall in &walls {
+        matrix[wall[0] as usize][wall[1] as usize] = State::Wall;
+    }
+
+    for guard in &guards {
+        let (row, col) = (guard[0] as usize, guard[1] as usize);
+        matrix[row][col] = State::Guarded;
+
+        for i in (0..row).rev() {
+            match matrix[i][col] {
+                State::UnGuarded => matrix[i][col] = State::Guarded,
+                State::Wall => break,
+                State::Guarded => (),
+            }
+        }
+
+        for i in row + 1..m {
+            match matrix[i][col] {
+                State::UnGuarded => matrix[i][col] = State::Guarded,
+                State::Wall => break,
+                State::Guarded => (),
+            }
+        }
+        for i in (0..col).rev() {
+            match matrix[row][i] {
+                State::UnGuarded => matrix[row][i] = State::Guarded,
+                State::Wall => break,
+                State::Guarded => (),
+            }
+        }
+        for i in col + 1..n {
+            match matrix[row][i] {
+                State::UnGuarded => matrix[row][i] = State::Guarded,
+                State::Wall => break,
+                State::Guarded => (),
+            }
+        }
+    }
+
+    let mut ret = 0;
+    for row in &matrix {
+        for n in row {
+            if *n == State::UnGuarded {
+                ret += 1;
+            }
+        }
+    }
+
+    ret
+}
+
+fn main() {
+    let m = 4;
+    let n = 6;
+    let guards = vec![vec![0, 0], vec![1, 1], vec![2, 3]];
+    let walls = vec![vec![0, 1], vec![2, 2], vec![1, 4]];
+    let ret = count_unguarded(m, n, guards, walls);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test() {
+    {
+        let m = 4;
+        let n = 6;
+        let guards = vec![vec![0, 0], vec![1, 1], vec![2, 3]];
+        let walls = vec![vec![0, 1], vec![2, 2], vec![1, 4]];
+        let ret = count_unguarded(m, n, guards, walls);
+        assert_eq!(ret, 7);
+    }
+    {
+        let m = 3;
+        let n = 3;
+        let guards = vec![vec![1, 1]];
+        let walls = vec![vec![0, 1], vec![1, 0], vec![2, 1], vec![1, 2]];
+        let ret = count_unguarded(m, n, guards, walls);
+        assert_eq!(ret, 4);
+    }
+}

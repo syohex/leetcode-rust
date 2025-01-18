11a176f2599dbfa1a09fa211a8f36d0ce57255a5
Shohei YOSHIDA <syohex@gmail.com>
2025-01-18 15:12:30 +0900

    Refactoring

--- challenge/202501/minimum_cost_to_make_at_least_one_valid_path_in_a_grid/src/main.rs
+++ challenge/202501/minimum_cost_to_make_at_least_one_valid_path_in_a_grid/src/main.rs
@@ -3,47 +3,29 @@ fn min_cost(grid: Vec<Vec<i32>>) -> i32 {
     use std::collections::BinaryHeap;
 
     let (rows, cols) = (grid.len(), grid[0].len());
-    let mut visited = vec![vec![false; cols]; rows];
+    let steps = [(0, 1, 1), (0, -1, 2), (1, 0, 3), (-1, 0, 4)];
+    let mut min_cost = vec![vec![std::i32::MAX; cols]; rows];
 
     let mut q = BinaryHeap::new();
     q.push((Reverse(0), (0, 0)));
+    min_cost[0][0] = 0;
 
     while let Some((Reverse(cost), (row, col))) = q.pop() {
-        if visited[row][col] {
-            continue;
-        }
-        visited[row][col] = true;
+        let sign = grid[row as usize][col as usize];
 
-        if row == rows - 1 && col == cols - 1 {
-            return cost;
-        }
-
-        let sign = grid[row][col];
-        if sign == 1 && col + 1 < cols {
-            q.push((Reverse(cost), (row, col + 1)));
-        } else if sign == 2 && col >= 1 {
-            q.push((Reverse(cost), (row, col - 1)));
-        } else if sign == 3 && row + 1 < rows {
-            q.push((Reverse(cost), (row + 1, col)));
-        } else if sign == 4 && row >= 1 {
-            q.push((Reverse(cost), (row - 1, col)));
-        }
-
-        if sign != 1 && col + 1 < cols {
-            q.push((Reverse(cost + 1), (row, col + 1)));
-        }
-        if sign != 2 && col >= 1 {
-            q.push((Reverse(cost + 1), (row, col - 1)));
-        }
-        if sign != 3 && row + 1 < rows {
-            q.push((Reverse(cost + 1), (row + 1, col)));
-        }
-        if sign != 4 && row >= 1 {
-            q.push((Reverse(cost + 1), (row - 1, col)));
+        for &(x, y, s) in &steps {
+            let (r, c) = (row + x, col + y);
+            if r >= 0 && r < rows as i32 && c >= 0 && c < cols as i32 {
+                let new_cost = cost + if sign == s { 0 } else { 1 };
+                if new_cost < min_cost[r as usize][c as usize] {
+                    min_cost[r as usize][c as usize] = new_cost;
+                    q.push((Reverse(new_cost), (r, c)));
+                }
+            }
         }
     }
 
-    unreachable!("never reach here");
+    min_cost[rows - 1][cols - 1]
 }
 
 fn main() {
@@ -78,5 +60,10 @@ fn test() {
         let grid = vec![vec![1, 2], vec![4, 3]];
         let ret = min_cost(grid);
         assert_eq!(ret, 1);
+    }
+    {
+        let grid = vec![vec![4]];
+        let ret = min_cost(grid);
+        assert_eq!(ret, 0);
     }
 }

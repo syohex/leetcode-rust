867503b0be269e83e443baa3d3ea16cfe51bfff6
Shohei YOSHIDA <syohex@gmail.com>
2025-04-08 11:28:05 +0900

    Add minimum number of operations to make elements in array distinct

---
+++ challenge/202504/minimum_number_of_operations_to_make_elements_in_array_distinct/src/main.rs
@@ -0,0 +1,44 @@
+fn minimum_operations(nums: Vec<i32>) -> i32 {
+    use std::collections::HashSet;
+
+    let mut s = HashSet::new();
+    for (i, n) in nums.into_iter().enumerate().rev() {
+        if s.contains(&n) {
+            return (i as i32 / 3) + 1;
+        }
+
+        s.insert(n);
+    }
+
+    0
+}
+
+fn main() {
+    let nums = vec![1, 2, 3, 4, 2, 3, 3, 5, 7];
+    let ret = minimum_operations(nums);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test() {
+    {
+        let nums = vec![1, 2, 3, 3];
+        let ret = minimum_operations(nums);
+        assert_eq!(ret, 1);
+    }
+    {
+        let nums = vec![1, 2, 3, 4, 2, 3, 3, 5, 7];
+        let ret = minimum_operations(nums);
+        assert_eq!(ret, 2);
+    }
+    {
+        let nums = vec![4, 5, 6, 4, 4];
+        let ret = minimum_operations(nums);
+        assert_eq!(ret, 2);
+    }
+    {
+        let nums = vec![6, 7, 8, 9];
+        let ret = minimum_operations(nums);
+        assert_eq!(ret, 0);
+    }
+}

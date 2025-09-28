2a4d5fab11ff71b6d0b242ae78279f5be8e3e8de
Shohei YOSHIDA <syohex@gmail.com>
2025-09-28 10:28:30 +0900

    accepted

--- challenge/202509/largest_perimeter_triangle/src/main.rs
+++ challenge/202509/largest_perimeter_triangle/src/main.rs
@@ -2,32 +2,14 @@ fn largest_perimeter(nums: Vec<i32>) -> i32 {
     let mut nums = nums;
     nums.sort_unstable();
 
-    let mut ret = 0;
     let len = nums.len();
-    for i in 0..(len - 2) {
-        for j in (i + 1)..(len - 1) {
-            let mut left = j + 1;
-            let mut right = len - 1;
-
-            while left <= right {
-                let mid = left + (right - left) / 2;
-                if mid == left {
-                    break;
-                }
-
-                if nums[i] + nums[j] > nums[mid] {
-                    left = mid;
-                } else {
-                    right = mid - 1;
-                }
-            }
-
-            if nums[i] + nums[j] > nums[left] {
-                ret = std::cmp::max(ret, nums[i] + nums[j] + nums[left]);
-            }
+    for i in (2..len).rev() {
+        if nums[i - 2] + nums[i - 1] > nums[i] {
+            return nums[i - 2] + nums[i - 1] + nums[i];
         }
     }
-    ret
+
+    0
 }
 
 fn main() {

946b30466323d70d62aa6597f17da3ec72f6dc95
Shohei YOSHIDA <syohex@gmail.com>
2026-03-01 10:00:34 +0900

    Add partitioning into minimum number of deci binary numbers

---
+++ challenge/202603/partitioning_into_minimum_number_of_deci_binary_numbers/src/main.rs
@@ -0,0 +1,18 @@
+fn min_partitions(n: String) -> i32 {
+    n.chars()
+        .map(|c| c.to_digit(10).unwrap() as i32)
+        .max()
+        .unwrap()
+}
+
+fn main() {
+    let ret = min_partitions("82734".to_string());
+    println!("ret={ret}");
+}
+
+#[test]
+fn test() {
+    assert_eq!(min_partitions("32".to_string()), 3);
+    assert_eq!(min_partitions("82734".to_string()), 8);
+    assert_eq!(min_partitions("27346209830709182346".to_string()), 9);
+}

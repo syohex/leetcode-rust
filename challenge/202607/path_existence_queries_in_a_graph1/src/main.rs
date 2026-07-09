a2edf24a5f87c79c5cb20bae3b2c2ef93298660e
Shohei YOSHIDA <syohex@gmail.com>
2026-07-09 12:12:37 +0900

    Accepted

--- challenge/202607/path_existence_queries_in_a_graph1/src/main.rs
+++ challenge/202607/path_existence_queries_in_a_graph1/src/main.rs
@@ -4,52 +4,21 @@ fn path_existence_queries(
     max_diff: i32,
     queries: Vec<Vec<i32>>,
 ) -> Vec<bool> {
-    use std::collections::VecDeque;
-
     let n = n as usize;
-    let mut graph = vec![vec![]; n];
+    let mut groups = vec![0; n];
 
-    for i in 0..n {
-        for j in (i + 1)..n {
-            if nums[j] - nums[i] > max_diff {
-                break;
-            }
-
-            graph[i].push(j);
-            graph[j].push(i);
+    for i in 1..n {
+        if nums[i] - nums[i - 1] <= max_diff {
+            groups[i] = groups[i - 1];
+        } else {
+            groups[i] = i;
         }
     }
 
-    let mut group = vec![n + 1; n];
-    for i in 0..n {
-        if group[i] != n + 1 {
-            continue;
-        }
-
-        group[i] = i;
-        let mut q = VecDeque::new();
-        q.push_back(i);
-
-        while !q.is_empty() {
-            let len = q.len();
-            for _ in 0..len {
-                let node = q.pop_front().unwrap();
-                for &next in &graph[node] {
-                    if group[next] == n + 1 {
-                        group[next] = i;
-                        q.push_back(next)
-                    }
-                }
-            }
-        }
-    }
-
-    let mut ret = vec![];
-    for q in queries {
-        ret.push(group[q[0] as usize] == group[q[1] as usize]);
-    }
-
-    ret
+    queries
+        .into_iter()
+        .map(|q| groups[q[0] as usize] == groups[q[1] as usize])
+        .collect()
 }
 
 fn main() {
@@ -63,6 +32,15 @@ fn main() {
 
 #[test]
 fn test() {
+    {
+        let n = 4;
+        let nums = vec![1, 1, 1, 1];
+        let max_diff = 0;
+        let queries = vec![vec![0, 1], vec![0, 2], vec![1, 3], vec![2, 3]];
+        let expected = vec![true, true, true, true];
+        let ret = path_existence_queries(n, nums, max_diff, queries);
+        assert_eq!(ret, expected);
+    }
     {
         let n = 2;
         let nums = vec![1, 3];

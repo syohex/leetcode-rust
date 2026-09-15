79904477a2cdb9c4009e5270de0ab853fdf0398e
Shohei YOSHIDA <syohex@gmail.com>
2026-09-15 09:16:22 +0900

    Add problem 4048

---
+++ problems/4048-count-value-with-equally-spaced-occurences1/src/main.rs
@@ -0,0 +1,25 @@
+fn count_special_integers(nums: Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+
+    let mut h = HashMap::new();
+    for (i, n) in nums.into_iter().enumerate() {
+        h.entry(n).or_insert(vec![]).push(i);
+    }
+
+    h.into_values()
+        .filter(|v| v.len() == 3)
+        .filter(|v| v[1] - v[0] == v[2] - v[1])
+        .count() as i32
+}
+
+fn main() {
+    let ret = count_special_integers(vec![1, 8, 1, 5, 1, 5, 8, 5]);
+    println!("ret={ret}");
+}
+
+#[test]
+fn test() {
+    assert_eq!(count_special_integers(vec![1, 8, 1, 5, 1, 5, 8, 5]), 2);
+    assert_eq!(count_special_integers(vec![8, 8, 8, 8]), 0);
+    assert_eq!(count_special_integers(vec![8, 6, 6, 8, 8]), 0);
+}

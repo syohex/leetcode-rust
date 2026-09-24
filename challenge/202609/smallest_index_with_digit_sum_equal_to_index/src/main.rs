b709d3d9cd98608508dc24f10c79739d87a28a14
Shohei YOSHIDA <syohex@gmail.com>
2026-09-24 11:38:29 +0900

    simplify code

--- challenge/202609/smallest_index_with_digit_sum_equal_to_index/src/main.rs
+++ challenge/202609/smallest_index_with_digit_sum_equal_to_index/src/main.rs
@@ -1,17 +1,19 @@
 fn smallest_index(nums: Vec<i32>) -> i32 {
-    match nums.into_iter().enumerate().position(|(i, mut n)| {
-        let mut sum = 0;
-        while n > 0 {
-            sum += n % 10;
-            n /= 10;
-        }
+    nums.into_iter()
+        .enumerate()
+        .position(|(i, mut n)| {
+            let mut sum = 0;
+            while n > 0 {
+                sum += n % 10;
+                n /= 10;
+            }
 
-        sum == i as i32
-    }) {
-        Some(v) => v as i32,
-        _ => -1,
-    }
+            sum == i as i32
+        })
+        .map(|i| i as i32)
+        .unwrap_or(-1)
 }
+
 fn main() {
     let ret = smallest_index(vec![1, 3, 2]);
     println!("ret={ret}");
